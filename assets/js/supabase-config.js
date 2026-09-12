c4ce90ff14c83960893786e813b86259507cfa06
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-09-12 20:51:45 +0300

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,6 @@
-window.ECHOES_SUPABASE_URL = "https://nlawhidxubnmoxtcxbiq.supabase.co";
+window.ECHOES_SUPABASE_URL = "https://nlawhidxubmnoxtcxbia.supabase.co";
+window.ECHOES_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_D0fon3030rpsPnuNyk88jg_vUjsvG2n";
 
-window.ECHOES_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_DOfon3O30rpsPnuNyk88jg_vUjsvG2n";
-
+if (typeof supabase !== 'undefined') {
+    window.db = supabase.createClient(window.ECHOES_SUPABASE_URL, window.ECHOES_SUPABASE_PUBLISHABLE_KEY);
+}
