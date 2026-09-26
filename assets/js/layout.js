c68c4c1fb3b46903a52ccf26ebe173e9b37336f0
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-09-26 22:36:50 +0300

    fix(global): preserve hero orbit after layout

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -63,12 +63,10 @@
             height=Math.max(height,(firstSection.offsetTop+firstSection.offsetHeight)-top);
             const oneHumanityOrbit=field.querySelector(".echoes-field-path-02");
             if(oneHumanityOrbit){
-              if(page==="home"){
-                const heroOrbit=oneHumanityOrbit.cloneNode(true);
-                heroOrbit.classList.add("echoes-hero-orbit");
-                heroOrbit.style.top="31vh";
-                field.appendChild(heroOrbit);
-              }
+              const heroOrbit=oneHumanityOrbit.cloneNode(true);
+              heroOrbit.classList.add("echoes-hero-orbit");
+              heroOrbit.style.top="31vh";
+              field.appendChild(heroOrbit);
               oneHumanityOrbit.style.top=((firstSection.offsetTop-top)+(firstSection.offsetHeight*.18))+"px";
             }
           }
